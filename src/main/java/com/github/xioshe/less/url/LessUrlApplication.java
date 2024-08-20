af8e93a134666fbdf0819a33c819747a48d67fcf
xioshe <xioshe@outlook.com>
2024-08-20 20:06:04 +0800

    initial commit

---
+++ src/main/java/com/github/xioshe/less/url/LessUrlApplication.java
@@ -0,0 +1,13 @@
+package com.github.xioshe.less.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessUrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/xioshe/less/url/LessUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xioshe.less.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
